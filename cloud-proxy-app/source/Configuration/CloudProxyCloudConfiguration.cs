d65ceb0e46e09caa4cd339e2c90e76b92cfebc30
Paul Gerard <paulfgerard@hotmail.com>
2020-08-26 14:11:09 +0100

    Addition of Blob connection string cloud configuration

---
+++ cloud-proxy-app/source/Configuration/CloudProxyCloudConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Glasswall.IcapServer.CloudProxyApp.Configuration
+{
+    public class CloudProxyCloudConfiguration : ICloudConfiguration
+    {
+        public string FileProcessingStorageConnectionString { get; set; }
+        public string FileProcessingStorageOriginalStoreName { get; set; }
+    }
+}
---
+++ cloud-proxy-app/source/Configuration/CloudProxyCloudConfigurationChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Glasswall.IcapServer.CloudProxyApp.Configuration
+{
+    public class CloudProxyCloudConfigurationChecker
+    {
+        public void CheckConfiguration(ICloudConfiguration configuration)
+        {
+            var configurationErrors = new List<string>();
+
+            if (string.IsNullOrEmpty(configuration.FileProcessingStorageConnectionString))
+                configurationErrors.Add($"'FileProcessingStorageConnectionString' configuration is missing");
+
+            if (string.IsNullOrEmpty(configuration.FileProcessingStorageOriginalStoreName))
+                configurationErrors.Add($"'FileProcessingStorageOriginalStoreName' configuration is missing");
+
+            if (configurationErrors.Any())
+            {
+                throw new InvalidApplicationConfigurationException(string.Join(',', configurationErrors));
+            }
+        }
+    }
+}
