b4f91e8fd51a957a1c1b5b28d25ccece3f9634b1
P Gerard <paulfgerard@gmail.com>
2021-01-22 09:25:24 +0000

    Copy across gw_rebuild configuration from mounted folder

--- opt/start.sh
+++ opt/start.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # copy across mounted configuration files and start c-icap service
-echo "INFO: Copying across mounted configuration"
-cp -t /usr/local/c-icap/etc /usr/local/c-icap/conf/c-icap.conf /usr/local/c-icap/gw-rebuid-conf/gw_rebuild.conf
 
-sleep 5
+MOUNTEDFILE=/usr/local/c-icap/conf/c-icap.conf
+if [ -f "$MOUNTEDFILE" ]; then
+	echo "INFO: Copying across mounted configuration"
+	cp -t /usr/local/c-icap/etc /usr/local/c-icap/conf/c-icap.conf /usr/local/c-icap/rebuild/gw_rebuild.conf
+else
+	echo "WARNING: No mounted configuration found, running with defaults"
+fi
+
+sleep 1
 echo "INFO: Starting up C-ICAP service"
 /usr/local/c-icap/bin/c-icap -N -D
